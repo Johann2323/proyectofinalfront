ad4d6830a24a8a06707744c26240c9f58cc67602
Johann2323 <johann.arizaga.est@tecazuay.edu.ec>
2023-03-15 00:30:39 -0500

    cambionuevo

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { libros } from '../registro-libro/libros';
+import { RegistroLibroService } from '../registro-libro/registro-libro.service';
+import { Router } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // Importar FormsModule
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +13,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  currentComponent: string = 'carrito';
+  public libros: libros = new libros();
+  libros1: libros[] = [];
+  constructor(private libroservice: RegistroLibroService, private router: Router) { }
 
   ngOnInit(): void {
+    this.libroservice.getLibros().subscribe(
+      libro => this.libros1 = libro
+      //libro => this.libros=libro
+    );
   }
-
+  showCarrito() {
+    this.currentComponent = 'carrito';
+  }
+  isFechaUltimoMes(fecha: string): boolean {
+    const fechaLibro = new Date(fecha);
+    const hoy = new Date();
+    const unMesAtras = new Date();
+    unMesAtras.setMonth(unMesAtras.getMonth() - 1);
+    return fechaLibro >= unMesAtras && fechaLibro <= hoy;
+  }
 }
