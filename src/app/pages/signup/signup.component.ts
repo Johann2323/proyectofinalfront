3ce74eb657acb879197ca4bc23fcac051db1fcfd
Liz <lizeth.orellana.est@tecazuay.edu.ec>
2023-03-16 00:15:15 -0500

    Login

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,4 +1,4 @@
-import  Swal  from 'sweetalert2';
+import Swal from 'sweetalert2';
 import { MatLegacySnackBar as MatSnackBar } from '@angular/material/legacy-snack-bar';
 import { UserService } from './../../services/user.service';
 import { Component, OnInit } from '@angular/core';
@@ -11,41 +11,44 @@ import { Component, OnInit } from '@angular/core';
 export class SignupComponent implements OnInit {
 
   public user = {
-    username : '',
-    password : '',
-    nombre : '',
-    apellido : '',
-    email : '',
-    telefono : ''
+    username: '',
+    password: '',
+    nombre: '',
+    apellido: '',
+    email: '',
+    telefono: ''
   }
 
-  constructor(private userService:UserService,private snack:MatSnackBar) { }
+  constructor(private userService: UserService, private snack: MatSnackBar) { }
 
   ngOnInit(): void {
   }
 
-  formSubmit(){
-    console.log(this.user);
-    if(this.user.username == '' || this.user.username == null){
-      this.snack.open('El nombre de usuario es requerido !!','Aceptar',{
-        duration : 3000,
-        verticalPosition : 'top',
-        horizontalPosition : 'right'
-      });
-      return;
-    }
-
-    this.userService.añadirUsuario(this.user).subscribe(
-      (data) => {
-        console.log(data);
-        Swal.fire('Usuario guardado','Usuario registrado con exito en el sistema','success');
-      },(error) => {
-        console.log(error);
-        this.snack.open('Ha ocurrido un error en el sistema !!','Aceptar',{
-          duration : 3000
+  formSubmit(contra1: string, contra2: string) {
+    if (contra1 == contra2) {
+      console.log(this.user);
+      if (this.user.username == '' || this.user.username == null) {
+        this.snack.open('El nombre de usuario es requerido !!', 'Aceptar', {
+          duration: 3000,
+          verticalPosition: 'top',
+          horizontalPosition: 'right'
         });
+        return;
       }
-    )
-  }
 
+      this.userService.añadirUsuario(this.user).subscribe(
+        (data) => {
+          console.log(data);
+          Swal.fire('Usuario guardado', 'Usuario registrado con exito en el sistema', 'success');
+        }, (error) => {
+          console.log(error);
+          this.snack.open('Ha ocurrido un error en el sistema !!', 'Aceptar', {
+            duration: 3000
+          });
+        }
+      )
+    }else{
+      Swal.fire('Error', 'Contraseñas no coinciden', 'error');
+  }
+}
 }
