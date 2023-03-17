8d30a6b91941fe2ecd48350f5cabf025fc36d556
renatzord <renato.llivisaca.est@tecazuay.edu.ec>
2023-03-17 15:26:26 -0500

    Vista Pagos/Compra

---
+++ src/app/pages/registro-admin/usuario.ts
@@ -0,0 +1,10 @@
+export class usuarios{
+    id?: number;
+    username?: string;
+    password?: string;
+    nombre?: string;
+    apellido?: string;
+    direccion?: string;
+    email?: string;
+    telefono?: number
+}
