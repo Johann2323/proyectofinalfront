1bfef1fd9901e60db7a92dae0f72d65c6d02ee0a
Liz <lizeth.orellana.est@tecazuay.edu.ec>
2023-03-17 23:09:16 -0500

    estandarizacion de ventanas

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -14,34 +14,14 @@ export class NavbarComponent implements OnInit {
   user: any = null;
   rol?: any = null;
   sinuser: boolean = true;
-  cliente: boolean = false;
-  admin: boolean = false;
+  cliente: boolean = true;
+  admin: boolean = true;
 
   constructor(public login: LoginService, private router: Router) { }
 
   ngOnInit(): void {
     this.isLoggedIn = this.login.isLoggedIn();
     this.user = this.login.getUser();
-  }
-
-  ngDoCheck(): void {
-    this.rol = this.login.getUserRole();
-    console.log(this.rol)
-    if (this.rol == null) {
-      this.sinuser = true;
-      this.cliente = false;
-      this.admin = false;
-    }else if(this.rol=="NORMAL"){
-      this.isLoggedIn=true;
-      this.sinuser = false;
-      this.cliente = true;
-      this.admin = false;
-    }else if( this.rol=="ADMIN"){
-      this.isLoggedIn=true;
-      this.sinuser = false;
-      this.cliente = false;
-      this.admin = true;
-    }
   }
 
   public logout() {
